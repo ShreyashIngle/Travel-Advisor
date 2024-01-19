42d1934f60625bca378178b046776f4412756af2
shreyashingle2004@gmail.com <shreyashingle2004@gmail.com>
2024-01-19 21:32:14 +0530

    Travel advisor project

---
+++ src/components/api/travelAdvisorAPI.js
@@ -0,0 +1,41 @@
+/* eslint-disable consistent-return */
+import axios from 'axios';
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'x-rapidapi-key': process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
+        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get('https://open-weather13.p.rapidapi.com/city/landon', {
+        params: { lat:lat, lon: lng },
+        headers: {
+          'x-rapidapi-key': process.env.REACT_APP_RAPID_API_WEATHER_API_KEY,
+          'x-rapidapi-host': 'open-weather13.p.rapidapi.com',
+        },
+      });
+
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
